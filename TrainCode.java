d2c096aaf597afda85f65f62d1a7d881a7aa459f
llj-qiqi <32973841+llj-qiqi@users.noreply.github.com>
2017-10-24 21:42:58 +0800

    commit first from my home

--- TrainCode.java
+++ TrainCode.java
@@ -1,11 +1,28 @@
-package llj;
-import java.lang.System.*;
-import java.lang.util.*;
+package javaTrain;
+import java.lang.System;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
-public class TrainCode
-{
-    public static void main(String[] args)
-    {
-    
-    }
+public class Main {
+
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext())
+		{
+			System.out.println(revert(sc.nextLine()));
+		}
+		
+		sc.close();
+		
+	}
+	
+	public static String revert(String str)
+	{
+		List<String> s1 = new ArrayList<>();
+		StringBuilder inputData = new StringBuilder(str);
+	    return inputData.reverse().toString();
+	}
+	
 }
